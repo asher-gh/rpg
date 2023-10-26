fe0f20dda1894954e37c7610ec4ae261373fc321
Asher <ashish.2017@hotmail.com>
2023-10-26 13:44:20 +0100

    Add random and pin generation with optional flags

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use clap::{Parser, ValueEnum};
+use rand::{thread_rng, Rng};
+
+// TODO: Memorable
+// TODO: Strength
 
 fn main() {
     let args = Args::parse();
+    let out = args
+        .p_type
+        .gen_pass(args.length, args.numbers, args.symbols, args.caps);
 
-    let out = match args.p_type {
-        PasswordType::Pin => "pin",
-        PasswordType::Random => "random",
-        PasswordType::Memorable => "memorable",
-    };
-
-    println!("type: {out} length: {}", args.length);
+    println!("{out}");
 }
 
 /// A random password generator
@@ -18,7 +19,7 @@ fn main() {
 struct Args {
     /// Password length
     #[arg(default_value_t = 16)]
-    length: u32,
+    length: u8,
 
     /// Password type
     #[arg(
@@ -49,4 +50,46 @@ enum PasswordType {
     Random,
     Pin,
     Memorable,
+}
+
+impl PasswordType {
+    #[allow(unused)]
+    fn gen_pass(&self, len: u8, nums: bool, symbols: bool, caps: bool) -> String {
+        let mut rng = thread_rng();
+        let upper_case: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let lower_case: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+        let digits: &[u8] = b"0123456789";
+        let special: &[u8] = b"!\"#$%&'*+,./:;=?@\\^`|~";
+        // let minus = "-";
+
+        let mut alphabet: Vec<u8> = match self {
+            PasswordType::Random => lower_case.into(),
+            PasswordType::Pin => digits.into(),
+            // TODO: memorable
+            _ => vec![],
+        };
+
+        if self == &Self::Random {
+            if caps {
+                alphabet = [&alphabet, upper_case].concat();
+            }
+            if nums {
+                alphabet = [&alphabet, digits].concat();
+            }
+            if symbols {
+                alphabet = [&alphabet, special].concat();
+            }
+        }
+
+        dbg!(&std::str::from_utf8(&alphabet));
+
+        let password: String = (0..len as usize)
+            .map(|_| {
+                let idx = rng.gen_range(0..alphabet.len());
+                alphabet[idx] as char
+            })
+            .collect();
+
+        password
+    }
 }
